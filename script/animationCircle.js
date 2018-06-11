615f0b51d78c99d19dbbd26f9ce5ac1d716822e0
funcgis <funcgis@gmail.com>
2018-06-11 17:31:06 +0800

    修复内存泄漏问题 开始编写接口测试代码

--- script/animationCircle.js
+++ script/animationCircle.js
@@ -2,7 +2,7 @@
  * @Author: funcgis 
  * @Date: 2018-06-10 15:22:24 
  * @Last Modified by: chenlong
- * @Last Modified time: 2018-06-11 16:10:12
+ * @Last Modified time: 2018-06-11 17:26:08
  * Three.js Animation Circle Plugin
  */
 
@@ -33,29 +33,34 @@ var AnimationCircle = function(scene, renderer, camera, position, rotation, radi
     this._circleRingOne = null;
     this._circleRingTwo = null;
 
+    this._materialMain = null;
+    this._materialRingOne = null;
+    this._materialRingTwo = null;
+
     //init Circle Animation Objects
     this._init = function(){
-        var circleBufferGeometry = new THREE.CircleBufferGeometry( this.radius, this.segments );
+        var circleMainGeometry = new THREE.CircleBufferGeometry( this.radius, this.segments );
         var circleRingOneGeometry = new THREE.CircleBufferGeometry( 0.001, this.segments );
         var circleRingTwoGeometry = new THREE.CircleBufferGeometry( this.radius/2, this.segments );
-        var materialMain = new THREE.MeshBasicMaterial( { color: color, opacity: 0.3, transparent: true, side: THREE.DoubleSide} );
-        var materialRingOne = new THREE.MeshBasicMaterial( { color: color, opacity: 0, transparent: true, side: THREE.DoubleSide} );
-        var materialRingTwo = new THREE.MeshBasicMaterial( { color: color, opacity: 0, transparent: true, side: THREE.DoubleSide} );
-        materialMain.needsUpdate=true;
-        materialRingOne.needsUpdate=true;
-        materialRingTwo.needsUpdate=true;
+        this._materialMain = new THREE.MeshBasicMaterial( { color: color, opacity: 0.3, transparent: true, side: THREE.DoubleSide} );
+        this._materialRingOne = new THREE.MeshBasicMaterial( { color: color, opacity: 0, transparent: true, side: THREE.DoubleSide} );
+        this._materialRingTwo = new THREE.MeshBasicMaterial( { color: color, opacity: 0, transparent: true, side: THREE.DoubleSide} );
 
-        this._circleMain = new THREE.Mesh( circleBufferGeometry, materialMain );
+        this._circleMain = new THREE.Mesh( circleMainGeometry, this._materialMain );
 
-        this._circleRingOne = new THREE.Mesh( circleRingOneGeometry, materialRingOne );
+        this._circleRingOne = new THREE.Mesh( circleRingOneGeometry, this._materialRingOne );
         this._circleRingOne.material.opacity = 0;
 
-        this._circleRingTwo = new THREE.Mesh( circleRingTwoGeometry, materialRingTwo );
+        this._circleRingTwo = new THREE.Mesh( circleRingTwoGeometry, this._materialRingTwo );
         this._circleRingTwo.material.opacity = 0;
         
-        this._circleMain.position = this.position;
-        this._circleRingOne.position = this.position;
-        this._circleRingTwo.position = this.position;
+        this._circleMain.position.set(this.position.x,this.position.y,this.position.z);
+        this._circleRingOne.position.set(this.position.x,this.position.y,this.position.z);
+        this._circleRingTwo.position.set(this.position.x,this.position.y,this.position.z);
+
+        // materialMain.needsUpdate=true;
+        // materialRingOne.needsUpdate=true;
+        // materialRingTwo.needsUpdate=true;
 
         this.scene.add( this._circleMain );
         this.scene.add( this._circleRingOne );
@@ -70,19 +75,23 @@ var AnimationCircle = function(scene, renderer, camera, position, rotation, radi
         if(that._ifAnimate){
             if(that._circleRingOne.geometry.boundingSphere.radius<that.radius){
                 var radiusOne = that._circleRingOne.geometry.boundingSphere.radius + speed;
+                that._circleRingOne.geometry.dispose ()
                 that._circleRingOne.geometry = new THREE.CircleBufferGeometry( radiusOne, that.segments );
                 that._circleRingOne.material.opacity = 1-radiusOne/that.radius;
             }                
             else if(that._circleRingOne.geometry.boundingSphere.radius>=that.radius){
+                that._circleRingOne.geometry.dispose ()
                 that._circleRingOne.geometry = new THREE.CircleBufferGeometry( 0.01, that.segments );
                 that._circleRingOne.material.opacity = 1;
             }
             if(that._circleRingTwo.geometry.boundingSphere.radius<that.radius){
                 var radiusTwo = that._circleRingTwo.geometry.boundingSphere.radius + speed;
+                that._circleRingTwo.geometry.dispose ()
                 that._circleRingTwo.geometry = new THREE.CircleBufferGeometry( radiusTwo, that.segments );
                 that._circleRingTwo.material.opacity = 1-radiusTwo/that.radius;
             }
             else if(that._circleRingTwo.geometry.boundingSphere.radius>=that.radius){
+                that._circleRingTwo.geometry.dispose ()
                 that._circleRingTwo.geometry = new THREE.CircleBufferGeometry( 0.01, that.segments );
                 that._circleRingTwo.material.opacity = 1;
             }
@@ -116,9 +125,9 @@ AnimationCircle.prototype.StopAnimation = function(){
  * @param {*} position 
  */
 AnimationCircle.prototype.SetPosition = function(position){
-    this._circleMain.position = position;
-    this._circleRingOne.position = position;
-    this._circleRingTwo.position = position;
+    this._circleMain.position.set(this.position.x,this.position.y,this.position.z);
+    this._circleRingOne.position.set(this.position.x,this.position.y,this.position.z);
+    this._circleRingTwo.position.set(this.position.x,this.position.y,this.position.z);
 }
 
 /**
@@ -126,9 +135,9 @@ AnimationCircle.prototype.SetPosition = function(position){
  * @param {*} rotation 
  */
 AnimationCircle.prototype.SetRotation = function(rotation){
-    this._circleMain.rotation = rotation;
-    this._circleRingOne.rotation = rotation;
-    this._circleRingTwo.rotation = rotation;
+    this._circleMain.rotation.set(this.rotation.x,this.rotation.y,this.rotation.z);
+    this._circleRingOne.rotation.set(this.rotation.x,this.rotation.y,this.rotation.z);
+    this._circleRingTwo.rotation.set(this.rotation.x,this.rotation.y,this.rotation.z);
 }
 
 /**
@@ -155,4 +164,15 @@ AnimationCircle.prototype.SetSegments = function(segments){
  */
 AnimationCircle.prototype.SetSpeed = function(speed){
     this.speed = speed;
+}
+
+/**
+ * Change Circle Animation Color
+ * @param {*} color 
+ */
+AnimationCircle.prototype.SetColor = function(color){
+    this.color = color;
+    this._circleMain.material.color = color;
+    this._circleRingOne.material.color = color;
+    this._circleRingTwo.material.color = color;
 }
